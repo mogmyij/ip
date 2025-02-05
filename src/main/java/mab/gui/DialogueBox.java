c0953e6379d0fdfe78be013fdfb3624b3f6a4299
mogmyij <foonglucas@gmail.com>
2025-02-05 23:32:25 +0800

    Add new package mab.gui to create GUI application for mab.

---
+++ src/main/java/mab/gui/DialogueBox.java
@@ -0,0 +1,18 @@
+package mab.gui;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogueBox extends HBox{
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialogueBox(String s, Image i) {
+        text = new Label(s);
+        displayPicture = new ImageView(i);
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
---
+++ src/main/java/mab/gui/Gui.java
@@ -0,0 +1,42 @@
+package mab.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class Gui extends Application {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/circuit.png"));
+
+    @Override
+    public void start(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        DialogueBox dialogBox = new DialogueBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
